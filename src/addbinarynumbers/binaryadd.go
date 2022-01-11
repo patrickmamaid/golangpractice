ebc0a51cf98e123787439ada27f14fa5f54679a6
Patrick Mamaid <patrickmamaid@gmail.com>
2022-01-10 17:07:41 -0800

    Binary addition

--- src/addbinarynumbers/binaryadd.go
+++ src/addbinarynumbers/binaryadd.go
@@ -1,15 +1,96 @@
 package main
 
+// prioritize readability
+
+import "fmt"
+
 func add(a string, b string) string {
 
-	
+	//retString := "0"
+	aLen := len(a)
+	bLen := len(b)
+	newB := b
+	newA := a
+
+	if aLen > bLen {
+		// pad b
+		fmt.Println("here")
+		for i:=0 ; i < (aLen - bLen); i++ {
+			newB = "0" + newB
+		}
+
+	}
+
+	if aLen < bLen {
+		fmt.Println("here2")
+		// pad a
+		for i:=0 ; i < ( bLen - aLen); i++ {
+			newA = "0" + newA
+		}
+	}
+
+	// continue since they are equal
 
 
-	return ""
+	// perform addition one by one
+
+	var carry int = 0
+	var retval string = ""
+	for i:= len(newA) - 1  ; i >= 0  ; i--{
+
+		// 1 + 0
+		if newA[i] == '0' {
+
+			if newB[i] == '0' {
+				// a 0 b 0 c 1
+				if carry == 1 {
+					retval = "1" + retval
+					carry = 0
+				}else {
+					// a 0 b 0 c 0
+					retval = "0" + retval
+				}
+			}else{ // b 1
+				// a 0 b 1 c 1
+				if carry == 1 {
+					retval = "0" + retval
+					carry = 1
+				}else { // a 0 b 1 c 0
+					retval = "1" + retval
+				}
+
+			}
+		}else{ // a == 1
+			if newB[i] == '0' {
+				// a 1 b 0 c 1
+				if carry == 1 {
+					retval = "1" + retval
+					carry = 0
+				}else { // a 1 b 0 c 0
+					retval = "0" + retval
+				}
+			}else{ // b == 1
+				if carry == 1 { // a 1 b 1 c 1
+					retval = "1" + retval
+					carry = 1
+				}else { // a 1 b 1 c 0
+					retval = "0" + retval
+					carry = 1
+				}
+			}
+		}
+	}
+
+
+	// process the final carry
+	if carry == 1 {
+		retval = "1" + retval
+	}
+	return retval
 }
 
 
 
 func main() {
-	add("1", "1")
+	fmt.Println(add("111", "1111"))
 }
